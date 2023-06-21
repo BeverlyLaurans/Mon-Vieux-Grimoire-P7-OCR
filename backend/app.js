afc4067ec5649bd5fcd0222c3934e0971b50cb43
BeverlyLaurans <beverly.laurans@free.fr>
2023-06-21 13:32:59 +0200

    Correction config connexion bdd

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const rateLimit = require('express-rate-limit');
 const config = require('./config/config');
 
 const app = express();
@@ -9,21 +10,34 @@ const booksRoutes = require('./routes/booksRoutes');
 const usersRoutes = require('./routes/usersRoutes');
 const path = require('path');
 
+console.log('Avant la connexion à MongoDB');
+
 mongoose.connect(`mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbHost}/?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+  console.log('Après la connexion à MongoDB');
+
 app.use(express.json());
 
-// Gestion CORS et méthodes
+// CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+
+// Limite de requêtes
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 100,
+  message: 'Trop de requêtes effectuées. Veuillez réessayer plus tard.',
+});
+
+app.use(limiter);
 
 // Chemin vers le répertoire pour les fichiers
 app.use('/images', express.static(path.join(__dirname, 'images')));
