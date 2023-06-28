7ada0fc1af4b68c2feaf68b39a4c64b802744b97
BeverlyLaurans <beverly.laurans@free.fr>
2023-06-28 16:36:20 +0200

    Vérification supplémentaire email User

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true, validate: {
+    validator: function (value) {
+      return value.includes('@');
+    },
+    message: 'Le champ email doit contenir le caractère "@"',
+  },
+},
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
