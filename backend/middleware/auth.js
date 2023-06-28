a325dfd98c66a4bdcd313610b2688fce2faa0ad3
BeverlyLaurans <beverly.laurans@free.fr>
2023-06-28 17:53:28 +0200

    Suppression tests commentaires

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,10 +7,13 @@ module.exports = (req, res, next) => {
        // Décodage et vérification du token
        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
        const userId = decodedToken.userId;
+
        // Stockage de l'id du user authentifié dans la propriété auth
        req.auth = {
            userId: userId
-       };
+       }
+    // }
+
 	next();
    } catch(error) {
        res.status(401).json({ error });
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,31 +1,3 @@
-// const multer = require('multer');
-
-// // Association des types aux formats correspondants
-// const MIME_TYPES = {
-//   'image/jpg': 'jpg',
-//   'image/jpeg': 'jpg',
-//   'image/png': 'png'
-// };
-
-// // Options de stockage des fichiers téléchargés
-// const storage = multer.diskStorage({
-//   // Gestion du dossier de destination
-//   destination: (req, file, callback) => {
-//     callback(null, 'images');
-//   },
-//   // Gestion du nom de fichier
-//   filename: (req, file, callback) => {
-//     const name = file.originalname.split(' ').join('_');
-//     // const extension = MIME_TYPES[file.mimetype];
-//     // callback(null, name + Date.now() + '.' + extension);
-//     callback(null, name + Date.now());
-//   }
-// });
-
-// module.exports = multer({storage: storage}).single('image');
-
-// _________________
-
 const multer = require('multer');
 
 // Association des types aux formats correspondants
