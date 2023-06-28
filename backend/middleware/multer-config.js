de064254c26c3cb6c810d677097afc057a5aefb9
BeverlyLaurans <beverly.laurans@free.fr>
2023-06-28 16:35:06 +0200

    Gestion des images multer et sharp

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,3 +1,31 @@
+// const multer = require('multer');
+
+// // Association des types aux formats correspondants
+// const MIME_TYPES = {
+//   'image/jpg': 'jpg',
+//   'image/jpeg': 'jpg',
+//   'image/png': 'png'
+// };
+
+// // Options de stockage des fichiers téléchargés
+// const storage = multer.diskStorage({
+//   // Gestion du dossier de destination
+//   destination: (req, file, callback) => {
+//     callback(null, 'images');
+//   },
+//   // Gestion du nom de fichier
+//   filename: (req, file, callback) => {
+//     const name = file.originalname.split(' ').join('_');
+//     // const extension = MIME_TYPES[file.mimetype];
+//     // callback(null, name + Date.now() + '.' + extension);
+//     callback(null, name + Date.now());
+//   }
+// });
+
+// module.exports = multer({storage: storage}).single('image');
+
+// _________________
+
 const multer = require('multer');
 
 // Association des types aux formats correspondants
@@ -15,10 +43,13 @@ const storage = multer.diskStorage({
   },
   // Gestion du nom de fichier
   filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
+    const originalName = file.originalname;
+    // Retrait de l'extension récupérée dans le nom du fichier
+    const nameWithoutExtension = originalName.split('.').slice(0, -1).join('.');
+    const name = nameWithoutExtension.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + '.' + extension);
   }
 });
 
-module.exports = multer({storage: storage}).single('image');
+module.exports = multer({ storage: storage }).single('image');
